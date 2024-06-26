3c420d34fd3d19434c2c715dcdc394bf2434e635
Victoria Chiang <vchiang18@gmail.com>
2024-06-26 15:16:01 -0700

    license function

--- netlify/functions/check-license.js
+++ netlify/functions/check-license.js
@@ -1,39 +1,58 @@
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
-exports.handler = async (event) => {
-  try {
-    if (!event.body) {
-      return {
-        statusCode: 400,
-        body: JSON.stringify({ error: "No request body" }),
-      };
-    }
-    const { subscriptionId } = JSON.parse(event.body);
+exports.handler = async (event, context) => {
+  const data = await (async () => {
+    const subs = await stripe.subscriptions.list();
+    return subs;
+  })();
 
-    try {
-      const subscription = await stripe.subscriptions.retrieve(subscriptionId);
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ data }),
+  };
 
-      if (subscription.status === "active") {
-        return {
-          statusCode: 200,
-          body: JSON.stringify({ licenseActive: true }),
-        };
-      } else {
-        return {
-          statusCode: 200,
-          body: JSON.stringify({ licenseActive: false }),
-        };
-      }
-    } catch (error) {
-      return {
-        statusCode: 500,
-        body: JSON.stringify({ error: error.message }),
-      };
-    }
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: error.message }),
-    };
-  }
+  // const subs = await stripe.subscriptions;
+  // console.log("subs: ", subs);
+  // return {
+  //   statusCode: 200,
+  // body: JSON.stringify({ subs }),
 };
+
+// try {
+//   if (!event.body) {
+//     console.log("event: ", event);
+//     return {
+//       statusCode: 400,
+//       body: JSON.stringify({
+//         error: "No request body",
+//         response: event.response,
+//       }),
+//     };
+//   }
+//   const { subscriptionId } = JSON.parse(event.body);
+//   try {
+//     const subscription = await stripe.subscriptions.retrieve(subscriptionId);
+//     console.log("event: ", event);
+//     if (subscription.status === "active") {
+//       return {
+//         statusCode: 200,
+//         body: JSON.stringify({ licenseActive: true }),
+//       };
+//     } else {
+//       return {
+//         statusCode: 200,
+//         body: JSON.stringify({ licenseActive: false }),
+//       };
+//     }
+//   } catch (error) {
+//     return {
+//       statusCode: 500,
+//       body: JSON.stringify({ error: error.message }),
+//     };
+//   }
+// } catch (error) {
+//   return {
+//     statusCode: 500,
+//     body: JSON.stringify({ error: error.message }),
+//   };
+// }
