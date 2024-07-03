e1b41accfe935841f42154234085987ad27451ee
Victoria Chiang <vchiang18@gmail.com>
2024-07-03 10:23:08 -0700

    fixed notAuthRoute to /

--- src/routes/BrowserRouter.js
+++ src/routes/BrowserRouter.js
@@ -1,60 +1,58 @@
-import { RouterProvider, createBrowserRouter} from 'react-router-dom';
-import { useAuth } from '../context/AuthContext';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
 import PlayEntry from "../components/PlayEntry";
 import PlayList from "../components/PlayList";
 import Login from "../components/Login";
 import ValueSettings from "../components/ValuesSettings";
-import { ProtectedRoute } from './Protected';
-import Layout from '../components/Layout';
-
+import { ProtectedRoute } from "./Protected";
+import Layout from "../components/Layout";
 
 const Routes = () => {
-    const { isAuthenticated } = useAuth();
-    const publicRoutes = [
+  const { isAuthenticated } = useAuth();
+  const publicRoutes = [
+    {
+      path: "/",
+      element: <Login />,
+    },
+  ];
+  const privateRoutes = [
+    {
+      path: "*",
+      element: <ProtectedRoute />,
+      children: [
         {
-            path: "/",
-            element: <Login />,
-        }
-    ];
-    const privateRoutes = [
-        {
-            path: '*',
-            element: <ProtectedRoute />,
-            children: [
-                {
-                    path: 'play-entry/',
-                    element: <PlayEntry />,
-                },
-                {
-                    path: 'play-list/',
-                    element: <PlayList />,
-                },
-                {
-                    path: "customize-values/",
-                    element: <ValueSettings />,
-                },
-            ],
-
-        }
-    ];
-    const notAuthRoutes = [
-        {
-            path:  '/login',
-            element: <Login />,
+          path: "play-entry/",
+          element: <PlayEntry />,
         },
-    ];
-    const router = createBrowserRouter([
         {
-            Component: Layout,
-            path: '/',
-            children: [
-                ...publicRoutes,
-                ...(!isAuthenticated ? notAuthRoutes : []),
-                ...privateRoutes,
-            ],
+          path: "play-list/",
+          element: <PlayList />,
         },
-    ]);
-    return <RouterProvider router={router} />;
+        {
+          path: "customize-values/",
+          element: <ValueSettings />,
+        },
+      ],
+    },
+  ];
+  const notAuthRoutes = [
+    {
+      path: "/",
+      element: <Login />,
+    },
+  ];
+  const router = createBrowserRouter([
+    {
+      Component: Layout,
+      path: "/",
+      children: [
+        ...publicRoutes,
+        ...(!isAuthenticated ? notAuthRoutes : []),
+        ...privateRoutes,
+      ],
+    },
+  ]);
+  return <RouterProvider router={router} />;
 };
 
 export default Routes;
