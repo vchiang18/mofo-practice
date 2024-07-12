2cb253153b09efd1d7c73ce2b3c7dd0a07eb248d
Nicholas Tomasso <tomassonicholas@gmail.com>
2024-07-12 15:14:02 -0400

    playing with drag and drop

--- src/components/test.js
+++ src/components/test.js
@@ -1,17 +1,33 @@
-// import Draggable from './Draggable'
 import  Draggable  from './Draggable'
+import { useState, useEffect } from 'react'
 
 export default function Test() {
+  const [data, setData] = useState(['Test1', 'Test2'])
+
+  useEffect(() => {
+  }, [data])
     return (
         <div className="p-4">
       <div
       className="flex flex-wrap items-center justify-center gap-24"
       >
+        {
+          data.map((item, index) => (
+            <div
+            key={index}
+            className="p-2"
+            >
+              <Draggable component={<h1>{item}</h1>}
+              index={index}
+              callBack={setData}
+              />
+            </div>
+          ))
+        }
 
-      <Draggable
-      component={<h1>Test</h1>}
 
-      />
+
+
       <div className="flex flex-wrap">
         <div className="p-2 w-full">
         </div>
